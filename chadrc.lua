08a7c204e97bb527a9e9311878ce78881e74d841
Dellan Muchengapadare <mactunechy@gmail.com>
2024-03-23 12:59:24 +0200

    nv-chad custom config

---
+++ chadrc.lua
@@ -0,0 +1,17 @@
+---@type ChadrcConfig
+local M = {}
+M.ui = {theme = 'catppuccin'}
+M.plugins = "custom.plugins"
+
+
+
+-- Custom options  will move them letter to another module
+local keymap = vim.api.nvim_set_keymap
+
+local opts = { noremap = true, silent = true }
+keymap('n', '<Up>', '<Nop>', opts)
+keymap('n', '<Down>', '<Nop>', opts)
+keymap('n', '<Left>', '<Nop>', opts)
+keymap('n', '<Right>', '<Nop>', opts)
+
+return M
